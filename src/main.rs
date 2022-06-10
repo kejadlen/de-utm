030de16a89ef9bca05a65d9e0206dc822784ac70
Alpha Chen <alpha@kejadlen.dev>
2022-06-10 16:28:43 -0700

    de-utm

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+use anyhow::{anyhow, Result};
+use url::Url;
+
+fn main() -> Result<()> {
+    let url = env::args()
+        .nth(1)
+        .ok_or_else(|| anyhow!("no url provided"))?;
+
+    let url = Url::parse(&url)?;
+    let no_utm: Vec<(_, _)> = url
+        .query_pairs()
+        .filter(|(k, _)| !k.starts_with("utm_"))
+        .collect();
+
+    let mut url = url.clone();
+    url.query_pairs_mut().clear().extend_pairs(no_utm);
+
+    print!("{}", &url);
+
+    Ok(())
+}
